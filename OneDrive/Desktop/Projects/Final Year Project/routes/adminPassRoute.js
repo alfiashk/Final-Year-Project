76f478e936616d0d1357eef063069fc7929d4b1c
Alfiya Shaikh <136909659+alfiashk@users.noreply.github.com>
2025-06-16 18:27:11 +0530

    Delete OneDrive/Desktop directory

--- OneDrive/Desktop/Projects/Final Year Project/routes/adminPassRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const adminPassController = require("../controller/adminPass")
-
-
-router.get('/', adminPassController.getAdminPass);
-router.post('/', adminPassController.postAdminPass);
-
-module.exports = router;
--- OneDrive/Desktop/Projects/Final Year Project/routes/cancelReserv.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const CancelReservController = require("../controller/cancelReserv");
-
-router.route('/')
-    .get(CancelReservController.getCancelReserv)
-    .post(CancelReservController.postCancelReserv);
-
-module.exports = router;
