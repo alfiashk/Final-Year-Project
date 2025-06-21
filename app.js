30edbdf977cfe4b47ced010dbe735143be4b89c1
Alfiya Shaikh <136909659+alfiashk@users.noreply.github.com>
2025-06-21 13:56:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ async function main() {
 }
 
 app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.engine("ejs", ejsMate);
 app.use(methodOverride("_method"));
